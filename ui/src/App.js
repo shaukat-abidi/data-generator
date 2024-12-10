6cf8f82b33334b3f10e44ee8dbf45cee1b990483
shaukat_abedi@hotmail.com <shaukat_abedi@hotmail.com>
2024-12-10 20:01:18 +1100

    UI skeleton in REACT

---
+++ ui/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+// Define table  
+const MainTable = {};
+
+// Add a new column
+const addColumn = {};
+
+// Update column details
+const updateColumn = {};
+
+// Remove a column
+const removeColumn = {}
+
+// Submit table schema to backend
+const submitTableSchema = {}
+
+
+export default MainTable;
